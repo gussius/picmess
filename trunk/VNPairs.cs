556814d5a80e59001f1e765b8294c2c1e0507ed6
gussius <gussius@3dfd5dbb-1382-4029-b9f1-6b95fef7a341>
2010-07-06 11:30:01 +0000

    Added a new struct for handling pairs of ints. Nearly got the obj loader finished.

---
+++ trunk/VNPairs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LearnShader
+{
+    struct VNPair
+    {
+        private int position;
+        private int normal;
+
+        public VNPair(int pos_ref, int norm_ref)
+        {
+            this.position = pos_ref;
+            this.normal = norm_ref;
+        }
+
+        public static bool operator ==(VNPair pair1, VNPair pair2)
+        {
+            if ((pair1.P == pair2.P) && (pair1.N == pair2.N))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(VNPair pair1, VNPair pair2)
+        {
+            if ((pair1.P != pair2.P) || (pair1.N != pair2.N))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "{" + this.P + ", " + this.N + "}";
+        }
+
+        public int P
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int N
+        {
+            get { return normal; }
+            set { normal = value; }
+        }
+    }
+}
