e24c7bc6d9c3ea4102ef7e2379f41d4f98e04f8b
gussius <gussius@3dfd5dbb-1382-4029-b9f1-6b95fef7a341>
2012-07-09 04:49:37 +0000

    Tidied up the SQL project. Working with DataRows instead of indexes. Posing a few problems.

--- SQL Project/SQLExample2/Form1.Designer.cs
+++ SQL Project/SQLExample2/Form1.Designer.cs
@@ -28,38 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea12 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tbFirstName = new System.Windows.Forms.TextBox();
             this.tbLastName = new System.Windows.Forms.TextBox();
             this.tbJobTitle = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.btnNext = new System.Windows.Forms.Button();
-            this.btnPrevious = new System.Windows.Forms.Button();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.lblRecordNo = new System.Windows.Forms.ToolStripStatusLabel();
             this.lblSpace = new System.Windows.Forms.ToolStripStatusLabel();
             this.lblAction = new System.Windows.Forms.ToolStripStatusLabel();
-            this.btnLast = new System.Windows.Forms.Button();
-            this.btnFirst = new System.Windows.Forms.Button();
             this.btnAddNew = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.dbGridView = new System.Windows.Forms.DataGridView();
-            this.dbBindSource = new System.Windows.Forms.BindingSource(this.components);
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1 = new System.Windows.Forms.Panel();
             this.statusStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dbGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dbBindSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -111,26 +97,6 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Job Title";
             // 
-            // btnNext
-            // 
-            this.btnNext.Location = new System.Drawing.Point(55, 78);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(75, 23);
-            this.btnNext.TabIndex = 1;
-            this.btnNext.Text = "NextRecord";
-            this.btnNext.UseVisualStyleBackColor = true;
-            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
-            // 
-            // btnPrevious
-            // 
-            this.btnPrevious.Location = new System.Drawing.Point(55, 48);
-            this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
-            this.btnPrevious.TabIndex = 0;
-            this.btnPrevious.Text = "Previous Record";
-            this.btnPrevious.UseVisualStyleBackColor = true;
-            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
-            // 
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -139,7 +105,7 @@
             this.lblAction});
             this.statusStrip.Location = new System.Drawing.Point(0, 415);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(667, 22);
+            this.statusStrip.Size = new System.Drawing.Size(393, 22);
             this.statusStrip.TabIndex = 0;
             this.statusStrip.Text = "statusStrip1";
             // 
@@ -152,7 +118,7 @@
             // lblSpace
             // 
             this.lblSpace.Name = "lblSpace";
-            this.lblSpace.Size = new System.Drawing.Size(531, 17);
+            this.lblSpace.Size = new System.Drawing.Size(257, 17);
             this.lblSpace.Spring = true;
             this.lblSpace.Text = "space holder";
             // 
@@ -162,29 +128,9 @@
             this.lblAction.Size = new System.Drawing.Size(42, 17);
             this.lblAction.Text = "Action";
             // 
-            // btnLast
-            // 
-            this.btnLast.Location = new System.Drawing.Point(55, 107);
-            this.btnLast.Name = "btnLast";
-            this.btnLast.Size = new System.Drawing.Size(75, 23);
-            this.btnLast.TabIndex = 0;
-            this.btnLast.Text = "Last Record";
-            this.btnLast.UseVisualStyleBackColor = true;
-            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
-            // 
-            // btnFirst
-            // 
-            this.btnFirst.Location = new System.Drawing.Point(55, 19);
-            this.btnFirst.Name = "btnFirst";
-            this.btnFirst.Size = new System.Drawing.Size(75, 23);
-            this.btnFirst.TabIndex = 0;
-            this.btnFirst.Text = "First Record";
-            this.btnFirst.UseVisualStyleBackColor = true;
-            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
-            // 
             // btnAddNew
             // 
-            this.btnAddNew.Location = new System.Drawing.Point(538, 19);
+            this.btnAddNew.Location = new System.Drawing.Point(287, 7);
             this.btnAddNew.Name = "btnAddNew";
             this.btnAddNew.Size = new System.Drawing.Size(75, 23);
             this.btnAddNew.TabIndex = 2;
@@ -194,7 +140,7 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(538, 48);
+            this.btnSave.Location = new System.Drawing.Point(287, 36);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 3;
@@ -204,7 +150,7 @@
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(538, 78);
+            this.btnUpdate.Location = new System.Drawing.Point(287, 66);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(75, 23);
             this.btnUpdate.TabIndex = 4;
@@ -214,7 +160,7 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(538, 107);
+            this.btnDelete.Location = new System.Drawing.Point(287, 95);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 23);
             this.btnDelete.TabIndex = 5;
@@ -226,68 +172,41 @@
             // 
             this.dbGridView.AllowUserToAddRows = false;
             this.dbGridView.AllowUserToDeleteRows = false;
+            this.dbGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dbGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dbGridView.Location = new System.Drawing.Point(12, 152);
             this.dbGridView.Name = "dbGridView";
             this.dbGridView.ReadOnly = true;
             this.dbGridView.RowHeadersVisible = false;
-            this.dbGridView.Size = new System.Drawing.Size(316, 246);
+            this.dbGridView.Size = new System.Drawing.Size(369, 246);
             this.dbGridView.TabIndex = 9;
-            this.dbGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dbGridView_CellContentClick);
-            // 
-            // chart1
-            // 
-            chartArea11.Name = "ChartArea1";
-            chartArea12.Name = "ChartArea2";
-            this.chart1.ChartAreas.Add(chartArea11);
-            this.chart1.ChartAreas.Add(chartArea12);
-            legend6.Name = "Legend1";
-            this.chart1.Legends.Add(legend6);
-            this.chart1.Location = new System.Drawing.Point(341, 152);
-            this.chart1.Name = "chart1";
-            series11.ChartArea = "ChartArea1";
-            series11.Legend = "Legend1";
-            series11.Name = "Series1";
-            series12.ChartArea = "ChartArea2";
-            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
-            series12.Legend = "Legend1";
-            series12.Name = "BarChart";
-            this.chart1.Series.Add(series11);
-            this.chart1.Series.Add(series12);
-            this.chart1.Size = new System.Drawing.Size(314, 246);
-            this.chart1.TabIndex = 10;
-            this.chart1.Text = "chart1";
+            this.dbGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dbGridView_CellClick);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.tbJobTitle);
             this.panel1.Controls.Add(this.tbFirstName);
             this.panel1.Controls.Add(this.tbLastName);
+            this.panel1.Controls.Add(this.btnDelete);
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnSave);
             this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnAddNew);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(186, 11);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(295, 125);
+            this.panel1.Size = new System.Drawing.Size(369, 125);
             this.panel1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(667, 437);
+            this.ClientSize = new System.Drawing.Size(393, 437);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.chart1);
             this.Controls.Add(this.dbGridView);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.btnAddNew);
-            this.Controls.Add(this.btnFirst);
-            this.Controls.Add(this.btnLast);
             this.Controls.Add(this.statusStrip);
-            this.Controls.Add(this.btnPrevious);
-            this.Controls.Add(this.btnNext);
             this.MinimumSize = new System.Drawing.Size(355, 277);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -295,8 +214,6 @@
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dbGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dbBindSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -312,12 +229,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnNext;
-        private System.Windows.Forms.Button btnPrevious;
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.ToolStripStatusLabel lblRecordNo;
-        private System.Windows.Forms.Button btnLast;
-        private System.Windows.Forms.Button btnFirst;
         private System.Windows.Forms.Button btnAddNew;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnUpdate;
@@ -329,8 +242,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn jobTypeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridView dbGridView;
-        private System.Windows.Forms.BindingSource dbBindSource;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Panel panel1;
     }
 }
--- SQL Project/SQLExample2/Form1.cs
+++ SQL Project/SQLExample2/Form1.cs
@@ -11,121 +11,106 @@ namespace SQLExample2
 {
     public partial class Form1 : Form
     {   
+        // Fields required for setting up access to an SQL database.
         System.Data.SqlClient.SqlConnection con;
-        DataSet ds1;
         System.Data.SqlClient.SqlDataAdapter da;
+        DataSet ds1;
+        
+
+        // Some helper fields.
         int maxRows = 0;
         int inc = 0;
-        string index = "0";
-        DataTable dTable = null;
+        DataRow selectedRow;
 
         public Form1()
         {
             InitializeComponent();
         }
+
         
+        // This section is only run once. i.e. Initialisation.
         private void Form1_Load(object sender, EventArgs e)
         {
-            btnNext.Focus();
+            // Create connection to database.
             con = new System.Data.SqlClient.SqlConnection();
-            con.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\angmcl\\Documents\\picmess\\SQL Project\\WorkerDB.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            con.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\angmcl\\Documents\\picmess\\" +
+                "SQL Project\\WorkerDB.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
 
+            // Create a new dataset (Subset of SQL database information in memory).
             ds1 = new DataSet();
-                        
+            
+            // Open the connection to the SQL database.
             con.Open();
 
-            string sql = "SELECT * From tblWorkers";
+            // This is the SQL query in string form.
+            string sql = "SELECT Worker_ID, RTRIM(first_Name), RTRIM(last_Name), RTRIM(job_Type) From tblWorkers";
+            // Create a new SqlDataAdapter for the SQL database. This give us access to the data in the DB based on
+            // the query.
             da = new System.Data.SqlClient.SqlDataAdapter(sql, con);
 
+            // Now we want to populate the memory resident dataset with the queried data from the SqlDataAdapter.
             da.Fill(ds1, "Workers");
+
+            // maxRows is initialised here to give us easy access to the number of rows in the "Workers" table in the
+            // dataset. Not sure is this is the best way to account for number of rows or if I should just interrogate
+            // the dataset.
             maxRows = ds1.Tables["Workers"].Rows.Count;
+
+            // Update the form with the first instance of data that we wish to view. Default: first row etc.
+            // Show server version started in status bar.
             UpdateStatusBar();
+            lblAction.Text = "SQL Server Version " + con.ServerVersion + " started";
+            
+            // Close the connection to the SQL database. Do we need to open the connection again during the life of
+            // this form?
             con.Close();
 
+            // Initialise the DataGridView in the form. Not sure where this form should reference.
             dbGridView.DataSource = ds1;
             dbGridView.DataMember = "Workers";
             dbGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
             dbGridView.ReadOnly = true;
             dbGridView.MultiSelect = false;
             dbGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dbGridView.AutoResizeColumns();
             dbGridView.CurrentCell = dbGridView[1, 0];
 
-            dTable = ds1.Tables["Workers"];
-            DataColumn[] keys = new DataColumn[1];
-            DataColumn column = new DataColumn();
-            column.DataType = System.Type.GetType("System.Int32");
-            column.ColumnName = "tableID";
-            dTable.Columns.Add(column);
-            keys[0] = column;
-            dTable.PrimaryKey = keys;
+            // Clears the formatting label in the statusbar.
+            lblSpace.Text = "";
 
-            ShowData();
+            // Initialise DataRow for use with selecting rows in the data grid.
+            selectedRow = GetSelectedRow();
 
-
-
+            // Fill text boxes with data from selected row.
+            FillTextBoxes();
         }
 
-        void ShowData()
+        // Gets the data row in the data set which the data grid view has selected.
+        private DataRow GetSelectedRow()
         {
-            //DataRow dRow = ds1.Tables["Workers"].Rows[inc];
-            DataRow dRow = dTable.Rows.Find(
-
-            tbFirstName.Text = dRow.ItemArray.GetValue(1).ToString().TrimEnd(' ');
-            tbLastName.Text = dRow.ItemArray.GetValue(2).ToString().TrimEnd(' ');
-            tbJobTitle.Text = dRow.ItemArray.GetValue(3).ToString().TrimEnd(' ');
-            dbGridView.CurrentCell = dbGridView.SelectedRows[0].Cells[0];
+            DataRowView selectedRowView = (DataRowView)dbGridView.SelectedRows[0].DataBoundItem;
+            return selectedRowView.Row;
         }
 
+        // This method fills the text boxes with the information from selected row of the datagrid
+        // based on the data set information.
+        void FillTextBoxes()
+        {
+            tbFirstName.Text = selectedRow.ItemArray.GetValue(1).ToString();
+            tbLastName.Text = selectedRow.ItemArray.GetValue(2).ToString();
+            tbJobTitle.Text = selectedRow.ItemArray.GetValue(3).ToString();
+        }
+
+        // Updates the status bar and clears action description.
         void UpdateStatusBar()
         {
             lblRecordNo.Text = "Record " + (inc + 1) + " of " + maxRows;
             lblAction.Text = "";
-            lblSpace.Text = "";
         }
 
         #region Button Functionality
-        private void btnNext_Click(object sender, EventArgs e)
-        {
-            if (inc < maxRows - 1)
-            {
-                inc++;
-                ShowData();
-                UpdateStatusBar();
-            }
-            else
-            {
-                lblAction.Text = "Already at last record";
-            }
-        }
 
-        private void btnPrevious_Click(object sender, EventArgs e)
-        {
-            if (inc > 0)
-            {
-                inc--;
-                ShowData();
-                UpdateStatusBar();
-            }
-            else
-            {
-                lblAction.Text = "Already at first record";
-            }
-        }
-
-        private void btnFirst_Click(object sender, EventArgs e)
-        {
-            inc = 0;
-            ShowData();
-            UpdateStatusBar();
-        }
-
-        private void btnLast_Click(object sender, EventArgs e)
-        {
-            inc = maxRows - 1;
-            ShowData();
-            UpdateStatusBar();
-        }
-
+        // Prepares the text boxes for adding another record.
         private void btnAddNew_Click(object sender, EventArgs e)
         {
             tbFirstName.Clear();
@@ -135,37 +120,47 @@ namespace SQLExample2
             lblRecordNo.Text = "Record " + (maxRows + 1) + " of " + (maxRows + 1);
         }
 
+        // Adds a new row to the data set based on the contents of the text boxes
+        // then updates the database via the data adapter based on the modified data set.
         private void btnSave_Click(object sender, EventArgs e)
         {
+            // Command builder to register changes though the data adapter.
             System.Data.SqlClient.SqlCommandBuilder cb;
             cb = new System.Data.SqlClient.SqlCommandBuilder(da);
 
+            // Create a row to add to the dataset.
             DataRow dRow = ds1.Tables["Workers"].NewRow();
             dRow[1] = tbFirstName.Text;
             dRow[2] = tbLastName.Text;
             dRow[3] = tbJobTitle.Text;
+            
+            // Add row to dataset and update database though data adapter.
             ds1.Tables["Workers"].Rows.Add(dRow);
             da.Update(ds1, "Workers");
 
+            // Increment helper fields to appropriate values, and update status bar.
             maxRows++;
             inc = maxRows - 1;
             UpdateStatusBar();
-            lblAction.Text = "Saved to database";
+            lblAction.Text = "Saved to database as Record " + inc;
 
+            // Refresh dataset to allow access to autonumber identity.
             ds1.Clear();
             da.Fill(ds1, "Workers");
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            // Create command builder.
             System.Data.SqlClient.SqlCommandBuilder cb;
             cb = new System.Data.SqlClient.SqlCommandBuilder(da);
 
-            System.Data.DataRow dRow2 = ds1.Tables["Workers"].Rows[inc];
+            // Creates a new data row based on the selected 
+            DataRow row = ds1.Tables["Workers"].Rows[inc];
 
-            dRow2[1] = tbFirstName.Text;
-            dRow2[2] = tbLastName.Text;
-            dRow2[3] = tbJobTitle.Text;
+            row[1] = tbFirstName.Text;
+            row[2] = tbLastName.Text;
+            row[3] = tbJobTitle.Text;
 
             da.Update(ds1, "Workers");
             lblAction.Text = "Record " + (inc + 1) + " Updated";
@@ -176,32 +171,35 @@ namespace SQLExample2
         {
             System.Data.SqlClient.SqlCommandBuilder cb;
             cb = new System.Data.SqlClient.SqlCommandBuilder(da);
-            int tempInc = inc + 1;
 
-            DialogResult dResult = MessageBox.Show("Are you sure you want to Delete record " + (inc + 1), "Delete", MessageBoxButtons.YesNo);
+            int row = ds1.Tables["Workers"].Rows.IndexOf(selectedRow);
+            DialogResult dResult = MessageBox.Show("Are you sure you want to Delete record " + (row + 1), "Delete", MessageBoxButtons.YesNo);
 
             if (dResult == DialogResult.Yes)
             {
-                ds1.Tables["Workers"].Rows[inc].Delete();
-                maxRows--;
-                inc = 0;
-                ShowData();
-                UpdateStatusBar();
-
+                ds1.Tables["Workers"].Rows[row].Delete();
                 da.Update(ds1, "Workers");
 
-                lblAction.Text = "Record " + tempInc + " Deleted";
+                dbGridView.CurrentCell = dbGridView[0, 0];
+                GetSelectedRow();
+                FillTextBoxes();
+                UpdateStatusBar();
+
+                lblAction.Text = "Record " + (row + 1) + " Deleted";
             }
             else
             {
                 lblAction.Text = "No records deleted";
             }
         }
+
         #endregion
 
-        private void dbGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dbGridView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            selectedRow = GetSelectedRow();
+            UpdateStatusBar();
+            FillTextBoxes();
         }
 
     }
